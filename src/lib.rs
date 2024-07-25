c1994dd4c08aa1c1c5c9e0ba8f0460fba5c4058c
Sebastian Raggl <P41353@fhooe.at>
2024-07-25 11:02:09 +0200

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![warn(clippy::all, rust_2018_idioms)]
-#![feature(iter_next_chunk, array_chunks,array_try_from_fn)]
+#![feature(iter_next_chunk, array_chunks, array_try_from_fn)]
 
 mod app;
 mod names;
 pub use app::NamesApp;
-
